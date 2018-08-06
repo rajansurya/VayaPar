aae0221716e8f4d2b1c754fd435089bc7dac26cc
rajan786 <rajan.gupta@skilrock.com>
2018-08-06 11:22:46 +0530

    secondcomd

--- app/src/main/java/com/example/rdx/vayparexpress/CallBackToLocation.java
+++
@@ -1,9 +0,0 @@
-package com.example.rdx.vayparexpress;
-
-/**
- * Created by RDX on 1/2/2018.
- */
-
-public interface CallBackToLocation {
-    public void returnDataToLocationSearch(String tag, String data);
-}
